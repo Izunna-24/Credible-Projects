2706fe36ecc88c86f53f99bf088d562f225af108
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-19 01:50:10 +0100

    Java

--- src/dtos/LoginRequest.java
+++ src/dtos/LoginRequest.java
@@ -1,7 +1,15 @@
 package dtos;
 
 public class LoginRequest {
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     private String  password;
     private String username;
-    private String confirmPassword;
+    private String reEnterPassword;
 }
