cc7a8ea40c7101a449c57d0acb0e8f2c9a8f8cda
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-19 01:58:42 +0100

    Java

---
+++ src/controllers/EntryController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import services.DiaryService;
+import services.DiaryServiceImp;
+
+public class EntryController {
+    private DiaryService diaryService = new DiaryServiceImp();
+
+    //public register()
+}
