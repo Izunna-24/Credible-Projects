b78c6768942a188494f94c7e29db1201cd24ec78
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-19 01:47:35 +0100

    Java

--- src/dtos/CreateEntryRequest.java
+++ src/dtos/CreateEntryRequest.java
@@ -2,6 +2,34 @@ package dtos;
 
 public class CreateEntryRequest {
     private String title;
-    private String body;
     private String author;
+    private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
