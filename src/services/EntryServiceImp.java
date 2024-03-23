b2c1c5017fdd8073aadb194ad635ef3962c93fa0
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-23 17:43:32 +0100

    Java

--- src/services/EntryServiceImp.java
+++ src/services/EntryServiceImp.java
@@ -8,13 +8,12 @@ import dtos.CreateEntryRequest;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EntryServiceImplementation implements EntryService {
+public class EntryServiceImp implements EntryService {
 
     private EntryRepository entryRepository = new EntryRepositoryImp();
     @Override
     public List<Entry> findByUsername(String username) {
         List<Entry> entries = new ArrayList<>();
-
         for (Entry entry : entryRepository.findAll()) {
             if (entry.getAuthor().equalsIgnoreCase(username)) {
                 entries.add(entry);
@@ -34,9 +33,19 @@ public class EntryServiceImplementation implements EntryService {
     }
 
     @Override
-    public void save(Entry entry) {
-    entryRepository.save(entry);
+    public void createEntry(CreateEntryRequest createEntryRequest) {
+        Entry entry = new Entry();
+        entry.setTitle(createEntryRequest.getTitle());
+        entry.setBody(createEntryRequest.getBody());
+        entry.setAuthor(createEntryRequest.getAuthor());
+        entryRepository.save(entry);
     }
+
+    @Override
+    public void updateEntry(Entry entry) {
+
+    }
+
 
     @Override
     public void delete(String entryId) {
