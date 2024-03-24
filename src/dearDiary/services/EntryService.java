cf1a3599791675cc6cd47d275ddd114f50c12ced
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-24 14:44:47 +0100

    Java

--- src/dearDiary/services/EntryService.java
+++ src/dearDiary/services/EntryService.java
@@ -2,14 +2,19 @@ package dearDiary.services;
 
 import dearDiary.data.models.Entry;
 import dearDiary.dtos.CreateEntryRequest;
+import dearDiary.dtos.DeleteEntryRequest;
+import dearDiary.dtos.UpdateEntryRequest;
 
 import java.util.List;
 
 public interface EntryService {
-    List<Entry> findByAuthor(String username);
+    List<Entry> findByAuthor(String author);
     Entry findById(int id);
     long count();
-   void createEntry(CreateEntryRequest createEntryRequest);
-   void updateEntry(Entry entry);
-   void delete(String entryId);
+   void createEntryWith(CreateEntryRequest createEntryRequest);
+   void updateEntryWith(UpdateEntryRequest updateEntry);
+   void deleteEntry(DeleteEntryRequest deleteRequest);
+   List<Entry> findAllEntries();
+
+    void save(Entry entry);
 }
