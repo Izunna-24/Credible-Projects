a55bc5f58d627dcc520e30f9e1a5d6684ffeee4f
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-24 14:42:01 +0100

    Java

---
+++ test/dearDiary/controllers/DiaryControllerTest.java
@@ -0,0 +1,40 @@
+package dearDiary.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiaryControllerTest {
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void createEntry() {
+    }
+
+    @Test
+    void updateEntry() {
+    }
+
+    @Test
+    void deleteEntry() {
+    }
+
+    @Test
+    void deleteDiary() {
+    }
+
+    @Test
+    void findAllEntries() {
+    }
+}
